50b209492cb684d425ab6515cd8a100e6ec30fb0
alastair rushworth <a.rushworth07@aberdeen.ac.uk>
2018-05-30 20:59:40 +0100

    Added tests from the moments and normtest packages

--- R/normality.R
+++ R/normality.R
@@ -1,23 +1,27 @@
 
 normality <- function(values){
-  # perform the tests from nor.test & stats
-  nor_tests <-  list(cvm.test(values), sf.test(values), pearson.test(values), 
-                     lillie.test(values), ad.test(values), shapiro.test(values))
+  if(!is.numeric(values)) stop("values must be a numeric vector")
+  # these are function names with similar output
+  fun_list  <- list(nortest::ad.test, moments::agostino.test, 
+                    normtest::ajb.norm.test, nortest::cvm.test, 
+                    normtest::frosini.norm.test, normtest::geary.norm.test, 
+                    normtest::hegazy1.norm.test, normtest::jb.norm.test, 
+                    moments::anscombe.test, moments::bonett.test,
+                    nortest::lillie.test, nortest::pearson.test, stats::shapiro.test, 
+                    nortest::sf.test, normtest::spiegelhalter.norm.test, normtest::wb.norm.test) 
+  # capture method, p.value and statistic for each when applied to values
+  norm_list <- lapply(fun_list, function(x)  suppressWarnings(try(lapply(list(values), x), silent = T))[[1]])
+  norm_list <- lapply(norm_list, function(x) c(x$method, x$statistic[1], x$p.value))
   # perform the test from mhde - capture to avoid printing any output
-  x         <- capture.output(mhd  <- mhde.test(values))
-  
-  # combine outputs into a single list
-  normal_test <- list()
-  # get stat and p-values from nor.tests
-  for(i in 1:6) normal_test[[i]] <- c(nor_tests[[i]]$method, nor_tests[[i]]$statistic, nor_tests[[i]]$p.value)
-  # get stat and p-values from shp test
-  shp              <- shapiro.test(values)
-  normal_test[[7]] <- c("Minimum Hellinger Distance normality test", shp[[1]], shp[[2]])
+  x         <- capture.output(mhd    <- mhde::mhde.test(values))
+  # extract the method, p.value and statistic and add to norm_list
+  norm_list[[length(norm_list) + 1]] <- c("Minimum Hellinger Distance normality test", mhd[[1]], mhd[[2]])
   # combine into a single tibble
-  normal_df           <- as_tibble(do.call("rbind", normal_test))
-  colnames(normal_df) <- c("Test", "Statistic", "p.value")
+  normal_df           <- as_tibble(do.call("rbind", norm_list))
+  colnames(normal_df) <- c("Test", "statistic", "p.value")
   # convert cols 2 & 3 to numeric
-  class(normal_df$Statistic) <-  class(normal_df$p.value) <- "numeric"
+  class(normal_df$statistic) <-  class(normal_df$p.value) <- "numeric"
+  # return in alphabetical order
+  normal_df <- normal_df[order(normal_df$Test), ]
   return(normal_df)
 }
-    
